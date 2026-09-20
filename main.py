8e584df8bc31880be2b6e575f980781a33efd91e
Lucas-AR <lucas-ar@hotmail.com>
2026-09-20 16:57:06 -0300

    commit 6

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.requests import Request
+from fastapi import FastAPI, Request, HTTPException
 from core.database import engine, Base
 from routers import auth, catalogo, pedidos, fidelidade, dashboard, pagamentos
+import uuid
+from datetime import datetime, timezone
+
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Rede Raizes do Nordeste", version="1.0")
 
-@app.exception_handler(Exception)
-async def erro_padrao(request: Request, exc: Exception):
-    return JSONResponse(status_code=500, content={"erro": "Erro interno", "mensagem": str(exc), "codigo": "INTERNAL_ERROR"})
+
+@app.exception_handler(HTTPException)
+async def http_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "error": "VALIDATION_ERROR",
+            "message": str(exc.detail),
+            "details": [{"field": "canalPedido", "issue": str(exc.detail)}],
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "path": str(request.url.path),
+            "requestId": str(uuid.uuid4())
+        }
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={
+            "error": "VALIDATION_ERROR",
+            "message": "Erro de validação",
+            "details": [{"field": err['loc'][-1], "issue": err['msg']} for err in exc.errors()],
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "path": str(request.url.path),
+            "requestId": str(uuid.uuid4())
+        }
+    )
 
 app.include_router(auth.router)
 app.include_router(catalogo.router)
